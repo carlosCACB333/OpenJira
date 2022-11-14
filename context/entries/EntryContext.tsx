3d229ab2282ef4fd494dce70e51ccc4def3e32d9
Carlos Antonio Castillo Blas <carloscacb333@gmail.com>
2022-11-14 17:34:51 -0500

    v0.1.2

--- context/entries/EntryContext.tsx
+++ context/entries/EntryContext.tsx
@@ -1,16 +1,10 @@
-import {
-  createContext,
-  FC,
-  PropsWithChildren,
-  useEffect,
-  useReducer,
-} from "react";
-import { Entry } from "../../interfaces";
-import { EntryReducer } from "./EntryReducer";
-import { ax } from "../../api";
-import { useSnackbar } from "notistack";
-import { AxiosError } from "axios";
-import { useRouter } from "next/router";
+import { createContext, FC, PropsWithChildren, useEffect, useReducer } from 'react';
+import { Entry } from '../../interfaces';
+import { EntryReducer } from './EntryReducer';
+import { ax } from '../../api';
+import { useSnackbar } from 'notistack';
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/router';
 
 interface EntryContextProps {
   entries: Entry[];
@@ -27,56 +21,54 @@ export const ENTRY_INITIAL_STATE: EntryState = {
   entries: [],
 };
 
-export const EntryContext = createContext<EntryContextProps>(
-  {} as EntryContextProps
-);
+export const EntryContext = createContext<EntryContextProps>({} as EntryContextProps);
 
 export const EntryProvider: FC<PropsWithChildren> = ({ children }) => {
   const [state, dispatch] = useReducer(EntryReducer, ENTRY_INITIAL_STATE);
   const { enqueueSnackbar } = useSnackbar();
   const router = useRouter();
   const addNewEntry = async (description: string) => {
-    const res = await ax.post<Entry>("/entries", { description });
+    const res = await ax.post<Entry>('/entries', { description });
     dispatch({
-      type: "[Entry] add-entry",
+      type: '[Entry] add-entry',
       payload: res.data,
     });
-    enqueueSnackbar("Se creó correctamente !!", {
-      variant: "info",
-      anchorOrigin: { horizontal: "right", vertical: "top" },
+    enqueueSnackbar('Se creó correctamente !!', {
+      variant: 'info',
+      anchorOrigin: { horizontal: 'right', vertical: 'top' },
     });
   };
 
   const updateEntry = async (entry: Entry) => {
-    const res = await ax.put<Entry>(`/entries/${entry._id}`, entry);
-    dispatch({ type: "[Entry] update-entry", payload: res.data });
-    enqueueSnackbar("Se actualizó correctamente !!", {
-      variant: "success",
-      anchorOrigin: { horizontal: "right", vertical: "top" },
+    const res = await ax.put<Entry>(`/entry/${entry._id}`, entry);
+    dispatch({ type: '[Entry] update-entry', payload: res.data });
+    enqueueSnackbar('Se actualizó correctamente !!', {
+      variant: 'success',
+      anchorOrigin: { horizontal: 'right', vertical: 'top' },
     });
   };
 
   const deleteEntry = async (id: string) => {
     try {
-      const res = await ax.delete(`/entries/${id}`);
-      dispatch({ type: "[Entry] delete-entry", payload: id });
-      enqueueSnackbar("Se eliminó correctamente !!", {
-        variant: "success",
-        anchorOrigin: { horizontal: "right", vertical: "top" },
+      const res = await ax.delete(`/entry/${id}`);
+      dispatch({ type: '[Entry] delete-entry', payload: id });
+      enqueueSnackbar('Se eliminó correctamente !!', {
+        variant: 'success',
+        anchorOrigin: { horizontal: 'right', vertical: 'top' },
       });
-      router.replace("/");
+      router.replace('/');
     } catch (error) {
       const e = error as AxiosError<{ msg: string }>;
-      enqueueSnackbar(e.response?.data.msg || "", {
-        variant: "error",
-        anchorOrigin: { horizontal: "right", vertical: "top" },
+      enqueueSnackbar(e.response?.data.msg || '', {
+        variant: 'error',
+        anchorOrigin: { horizontal: 'right', vertical: 'top' },
       });
     }
   };
 
   const refreshEntries = () => {
-    ax.get<Entry[]>("/entries").then((res) => {
-      dispatch({ type: "[Entry] load-entry", payload: res.data });
+    ax.get<Entry[]>('/entries').then((res) => {
+      dispatch({ type: '[Entry] load-entry', payload: res.data });
     });
   };
 
@@ -85,9 +77,7 @@ export const EntryProvider: FC<PropsWithChildren> = ({ children }) => {
   }, []);
 
   return (
-    <EntryContext.Provider
-      value={{ ...state, addNewEntry, updateEntry, deleteEntry }}
-    >
+    <EntryContext.Provider value={{ ...state, addNewEntry, updateEntry, deleteEntry }}>
       {children}
     </EntryContext.Provider>
   );
--- pages/api/entry/[id].ts
+++ pages/api/entry/[id].ts
@@ -1,32 +1,29 @@
-import mongoose from "mongoose";
-import { Entry as IEntry } from "../../../../interfaces";
-import { db } from "../../../../database";
-import { Entry } from "../../../../models";
-import { NextApiRequest, NextApiResponse } from "next";
+import mongoose from 'mongoose';
+import { Entry as IEntry } from '../../../interfaces';
+import { db } from '../../../database';
+import { Entry } from '../../../models';
+import { NextApiRequest, NextApiResponse } from 'next';
 type Data =
   | {
       msg: string;
     }
   | IEntry;
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const { id } = req.query;
 
   if (!mongoose.isValidObjectId(id)) {
-    return res.status(400).json({ msg: "No es un id válido" });
+    return res.status(400).json({ msg: 'No es un id válido' });
   }
   switch (req.method) {
-    case "GET":
+    case 'GET':
       return getEntryByID(req, res);
-    case "PUT":
+    case 'PUT':
       return updateEntry(req, res);
-    case "DELETE":
+    case 'DELETE':
       return deleteEntry(req, res);
     default:
-      return res.status(400).json({ msg: "Método no definido" });
+      return res.status(400).json({ msg: 'Método no definido' });
   }
 }
 
@@ -35,15 +32,10 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const entryToUpdate = await Entry.findById(req.query.id);
   if (!entryToUpdate) {
     db.disconnect();
-    return res
-      .status(400)
-      .json({ msg: "No se encuentra la entrada con ese id" });
+    return res.status(400).json({ msg: 'No se encuentra la entrada con ese id' });
   }
 
-  const {
-    description = entryToUpdate.description,
-    status = entryToUpdate.status,
-  } = req.body;
+  const { description = entryToUpdate.description, status = entryToUpdate.status } = req.body;
 
   try {
     const entryUpdated = await Entry.findByIdAndUpdate(
@@ -55,7 +47,7 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     return res.status(200).json(entryUpdated!);
   } catch {
     await db.disconnect();
-    return res.status(400).json({ msg: "Error al actualizar" });
+    return res.status(400).json({ msg: 'Error al actualizar' });
   }
 };
 const deleteEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
@@ -65,27 +57,22 @@ const deleteEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     const entryDeleted = await Entry.findByIdAndDelete(req.query.id);
     await db.disconnect();
     if (!entryDeleted) {
-      return res.status(400).json({ msg: "Error al eliminar" });
+      return res.status(400).json({ msg: 'Error al eliminar' });
     }
     return res.status(200).json(entryDeleted);
   } catch (error) {
     console.log(error);
     db.disconnect();
-    return res.status(400).json({ msg: "Error al eliminar" });
+    return res.status(400).json({ msg: 'Error al eliminar' });
   }
 };
 
-const getEntryByID = async (
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) => {
+const getEntryByID = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   await db.connect();
   const entry = await Entry.findById(req.query.id);
   db.disconnect();
   if (!entry) {
-    return res
-      .status(400)
-      .json({ msg: "No se encuentra la entrada con ese id" });
+    return res.status(400).json({ msg: 'No se encuentra la entrada con ese id' });
   }
   return res.status(200).json(entry);
 };
